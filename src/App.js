840af85c1a448877a3e624f1a65deab43b0342c8
DESKTOP-GOK3KSC\DELL <natalia.nideck@gmail.com>
2025-06-21 15:34:01 -0400

    Trying useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import Nome from "./components/Nome"; 
+import { useState } from "react"; 
 
-function App() {
-  return (
+function App(){
+  const [nome, setNome] = useState("João");
+  const [idade, setIdade] = useState(15);
+
+ function handleChangeNome(novoNome, novaIdade){
+        setNome(novoNome);
+        setIdade(novaIdade);
+ }  
+
+  return(
     <div>
       <h1>
-        Welcome!
+        Component App
       </h1>
-
+      <Nome aluno={nome} idade={idade} />
+      <br/>
+      <button onClick={() => {handleChangeNome('José', 29)}}>
+        Mudar nome
+      </button>
     </div>
-  );
+  
+  )
+
+  
 }
 
 export default App;
+
---
+++ src/components/Nome.js
@@ -0,0 +1,17 @@
+function Nome(props){
+  return(
+    <span>
+usuario: {props.aluno} tem {props.idade} anos    
+    </span>
+  )
+}
+
+/* function Nome({ aluno} ){
+  return(
+    <span>
+usuario: {aluno}  
+    </span>
+  )
+} */
+
+export default Nome;
