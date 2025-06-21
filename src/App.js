fe0e4f9d8704200674bfa07c585c6784af6ff2b5
DESKTOP-GOK3KSC\DELL <natalia.nideck@gmail.com>
2025-06-21 16:42:14 -0400

    Using useState to create To Do list

--- src/App.js
+++ src/App.js
@@ -1,72 +1,41 @@
+import { useState } from "react";
 
-import { useState } from "react"; 
+function App() {
+  const [input, setInput] = useState("");
+  const [tarefas, setTarefas] = useState(["Pagar contas", "Estudar React"]);
 
-function App(){
-const [nome, setNome] = useState("");
-const [email, setEmail] = useState("");
-const [idade, setIdade] = useState("");
+  function handleRegister(e) {
+    e.preventDefault();
+    setTarefas(input ? [...tarefas, input] : tarefas);
+    setInput("");
+  }
 
-const [user, setUser] = useState({});
-
-function handleRegister(e) {
-  e.preventDefault();
-alert("Usuário cadastrado com sucesso!");
-// console.log("Usuário cadastrado com sucesso!");
-
-  setUser({
-    nome: nome,
-    email: email,
-    idade: idade
-  })
-}
-
-  return(
+  return (
     <div>
       <h1>Cadastrando usuário</h1>
       <form onSubmit={handleRegister}>
-       <label>Nome:
-        </label>
-        <br/>
-        <input placeholder="Digite seu nome" value={nome} onChange={(e) => setNome(e.target.value)}
+        <label>Nome da tarefa:</label>
+        <br />
+        <input
+          placeholder="Digite sua tarefa"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         ></input>
-        
-        <br/>
-           <label>Email:
-        </label>
-        <br/>
-        <input placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <br/>
-           <label>Idade:
-        </label>
-        <br/>
-        <input placeholder="Digite seu Idade" value={idade} onChange={(e) => setIdade(e.target.value)}
-        ></input>
-        <br/>
-        <button type="submit" onClick={handleRegister}>Registrar</button>
+        <br />
+        <br />
+        <button type="submit" onClick={handleRegister}>
+          Registrar
+        </button>
       </form>
-       <br/>
-        <br/>
-        <div>
-          <span>
-            Bem vindo: {user.nome}
-          </span>
-          <br/>
-          <span>
-            Idade: {user.idade}
-          </span>
-          <br/>
-          <span>
-            Email: {user.email}
-          </span>
-
-        </div>
-
+      <br />
+      <br />
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
     </div>
-  )
-
-  
+  );
 }
 
 export default App;
-
